79e7c1db0de6c1e230b2a76e96062ffe82bba2b8
Yan Conigliaro <yan.conigliaro@etu.univ-cotedazur.fr>
2019-02-26 10:37:56 +0100

    exo4 ticketId : get, delete, put

--- app/api/tickets/index.js
+++ app/api/tickets/index.js
@@ -3,6 +3,9 @@ const { Ticket } = require('../../models');
 
 const router = new Router();
 router.get('/', (req, res) => res.status(200).json(Ticket.get()));
+router.get('/:ticketId', (req, res) => res.status(200).json(Ticket.getById(req.params.ticketId)));
+router.delete('/:ticketId', (req, res) => res.status(200).json(Ticket.delete(req.params.ticketId)));
+router.put('/:ticketId', (req, res) => res.status(200).json(Ticket.update(req.params.ticketId, req.body)));
 router.post('/', (req, res) => {
   try {
     const ticket = Ticket.create(req.body);

8773542c846cb2acf1e243d3f3c8e686ef1c76de
Yan Conigliaro <yan.conigliaro@etu.univ-cotedazur.fr>
2019-03-05 11:14:22 +0100

    exo 6 : attach student to ticket

--- app/api/tickets/index.js
+++ app/api/tickets/index.js
@@ -1,11 +1,33 @@
 const { Router } = require('express');
 const { Ticket } = require('../../models');
+const { Student } = require('../../models');
+
+function attachStudent(ticket) {
+  const ticketMod = ticket;
+  ticketMod.student = Student.getById(ticket.studentId);
+  return ticketMod;
+}
 
 const router = new Router();
-router.get('/', (req, res) => res.status(200).json(Ticket.get()));
-router.get('/:ticketId', (req, res) => res.status(200).json(Ticket.getById(req.params.ticketId)));
-router.delete('/:ticketId', (req, res) => res.status(200).json(Ticket.delete(req.params.ticketId)));
-router.put('/:ticketId', (req, res) => res.status(200).json(Ticket.update(req.params.ticketId, req.body)));
+router.get('/', (req, res) => {
+  const ticketList = Ticket.get().map(
+    e => attachStudent(e),
+  );
+  res.status(200).json(ticketList);
+});
+
+router.get('/:ticketId', (req, res) => res.status(200).json(
+  attachStudent(Ticket.getById(req.params.ticketId)),
+));
+
+router.delete('/:ticketId', (req, res) => res.status(200).json(
+  Ticket.delete(req.params.ticketId),
+));
+
+router.put('/:ticketId', (req, res) => res.status(200).json(
+  attachStudent(Ticket.update(req.params.ticketId, req.body)),
+));
+
 router.post('/', (req, res) => {
   try {
     const ticket = Ticket.create(req.body);
--- app/models/ticket.model.js
+++ app/models/ticket.model.js
@@ -4,5 +4,5 @@ const BaseModel = require('../utils/base-model.js');
 module.exports = new BaseModel('Ticket', {
   title: Joi.string().required(),
   date: Joi.date().required(),
-  student: Joi.string().required(),
+  studentId: Joi.number().integer().required(),
 });
